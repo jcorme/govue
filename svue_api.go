f3f3d1b4f6fd9928d62a5cdab518810ebea98427
Jason Chen <jason+dev@ga.jcor.me>
2015-09-19 13:29:41 -0700

    Add XML request bodies for signing in and requesting grades

--- svue_api.go
+++ svue_api.go
@@ -17,6 +17,37 @@ const (
 	soapAction   = "http://edupoint.com/webservices/ProcessWebServiceRequest"
 )
 
+const (
+	signInRequestBody = `<?xml version="1.0" encoding="utf-8"?>
+		<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<ProcessWebServiceRequest xmlns="http://edupoint.com/webservices/">
+					<userID>%s</userID>
+					<password>%s</password>
+					<skipLoginLog>0</skipLoginLog>
+					<parent>0</parent>
+					<webServiceHandleName>PXPWebServices</webServiceHandleName>
+					<methodName>ChildList</methodName>
+					<paramStr/>
+				</ProcessWebServiceRequest>
+			</soap:Body>
+		</soap:Envelope>`
+	getGradesRequestBody = `<?xml version="1.0" encoding="utf-8"?>
+		<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<ProcessWebServiceRequest xmlns="http://edupoint.com/webservices/">
+					<userID>%s</userID>
+					<password>%s</password>
+					<skipLoginLog>1</skipLoginLog>
+					<parent>0</parent>
+					<webServiceHandleName>PXPWebServices</webServiceHandleName>
+					<methodName>Gradebook</methodName>
+					<paramStr>&lt;Parms&gt;&lt;ChildIntID&gt;0&lt;/ChildIntID&gt;&lt;/Parms&gt;</paramStr>
+				</ProcessWebServiceRequest>
+			</soap:Body>
+		</soap:Envelope>`
+)
+
 func callApi(body io.Reader) (*SVUEResponse, error) {
 	req, err := newSVueRequest(body)
 
